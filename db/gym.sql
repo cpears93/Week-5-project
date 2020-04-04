d5473c1ef75e45da2c5701923b0470900e01aeb6
Christopher Pearson <cpearson3140@gmail.com>
2020-04-04 15:18:36 +0100

    Populated SQL database

---
+++ db/gym.sql
@@ -0,0 +1,24 @@
+DROP TABLE customers
+DROP TABLE instructors
+DROP TABLE fitness_class
+
+CREATE TABLE customers
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  type VARCHAR(255)
+);
+
+CREATE TABLE instructors
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  type VARCHAR(255)
+);
+
+CREATE TABLE fitness_class
+(
+  id SERIAL primary key
+  customer_id INT references customers(id),
+  instructor_id INT references instructors(id)
+)
