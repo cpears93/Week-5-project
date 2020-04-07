da52f5652553fd45c487b429a2f297a245f6b05a
Christopher Pearson <cpearson3140@gmail.com>
2020-04-07 16:16:04 +0100

    Updated views

--- controllers/fitness_controller.rb
+++ controllers/fitness_controller.rb
@@ -4,11 +4,11 @@ require_relative( '../models/fitness.rb' )
 also_reload( '../models/*' )
 
 get '/fitness' do
-  @fitness_class = Fitness.all()
-  ern ( :"fitness/index")
+  @fitness = Fitness.all()
+  erb (:"fitness_class/index")
 end
 
 get 'fitness/:id' do
-  @fitness_class = Fitness.find(params['id'].to_i)
-  erb (:"fitness/show")
+  @fitness = Fitness.find(params['id'].to_i)
+  erb (:"fitness_class/show")
 end
